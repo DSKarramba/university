15b00ecbe2081afdda1b5783a5e6b44ba8bdeb92
Vova Abdrakhmanov <369565@gmail.com>
2014-05-27 07:09:37 +0400

    add elements table

--- basics of modeling/deploy.py
+++ basics of modeling/deploy.py
@@ -1,10 +1,25 @@
 import os
 import sys
 
+
+def symbol(z):
+    table = ["H", "He",
+             "Li", "Be", "B", "C", "N", "O", "F", "Ne",
+             "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar",
+             "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni",
+             "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr",
+             "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd",
+             "Ag", "Cd", "In", "Sn"]
+    return table[z-1]
+
+
 def make_lab(number, student, z1, z2):
     os.system("mkdir test/lab%d" % number)
     with open("pre/lab%d.tex" % number, "r") as f:
-        pre = f.read() % student
+        if number == 1:
+            pre = f.read() % student
+        else:
+            pre = f.read() % (symbol(z1), symbol(z2), student)
     with open("text/lab%d.tex" % number, "r") as f:
         text = f.read()
     with open("post/lab.tex", "r") as f:
@@ -19,8 +34,13 @@ def make_lab(number, student, z1, z2):
                   (number, sys.executable, z1, z2))
 
 
+def make_work():
+    pass
+
+
 def usage():
     print("./deploy.py Фамилия И. О. z1 z2")
+
 
 if __name__ == '__main__':
     if os.path.exists("test"):
