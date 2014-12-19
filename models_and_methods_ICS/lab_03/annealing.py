898b85b21d12428a67aa78a6820a1945cce64e31
Чечеткин Илья <illaech@gmail.com>
2014-12-19 16:36:17 +0300

    [M&M_ICS] cleaning 3rd lab

--- models_and_methods_ICS/lab_03/annealing.py
+++ models_and_methods_ICS/lab_03/annealing.py
@@ -16,9 +16,6 @@ x0 = (random() - 0.5) * r
 y0 = (random() - 0.5) * r
 f0 = func(x0, y0)
 
-fl = [f0]
-xl = [x0]
-
 for i in range(n):
   x = (random() - 0.5) * r + x0
   y = (random() - 0.5) * r + y0
@@ -26,16 +23,11 @@ for i in range(n):
   if f < f0:
     x0, y0, f0 = x, y, f
   else:
-    if f - f0 < 0:
-      p = 1
-    else:
-      p = np.exp(-(f - f0) / t)
+    p = np.exp(-(f - f0) / t)
     if random() < p:
       #print '  jump from {1:.5f} to {0:.5f}'.format(f, f0)
       x0, y0, f0 = x, y, f
   r -= r / n
   t -= t / n
-  xl.append(x0)
-  fl.append(f0)
 
 print x0, y0, f0
